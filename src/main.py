523e6d0fe005178469d9a554cbb3493a3107e2ac
DynamicSquid <apostolu240@gmail.com>
2020-12-30 18:16:01 -0700

    working on making it all pyhton

--- src/main.py
+++ src/main.py
@@ -1,19 +1,61 @@
 import sys
 import requests
 
-def scrape(web_tab, tab):
-  page = requests.get('https://github.com/' + web_tab)
+name = 'DynamicSquid'#input('username: ')
 
-  if page.status_code == 404:
-    print('user not found')
-    sys.exit(1)
+website = requests.get('https://github.com/' + name)
 
-  file = open('../data/' + tab + '.txt', 'w')
-  file.write(page.content.decode('utf-8'))
-  file.close()
+if website.status_code == 404:
+	print('user not found')
+	sys.exit(1)
 
-scrape(sys.argv[1], 'profile')
-scrape(sys.argv[1] + '?tab=followers', 'followers')
-scrape(sys.argv[1] + '?tab=following', 'following')
-scrape(sys.argv[1] + '?tab=repositories', 'repos')
-scrape(sys.argv[1] + '?tab=stars', 'stars')
+def extract_data(_page, tag, endTag, inc):
+	website = requests.get('https://github.com/' + name + '/?tab=' + _page)
+	page = website.content.decode('utf-8')
+
+	tag_index = page.find(tag)
+	while tag_index != -1:
+		endIndex = page.find(endTag, tag_index + inc)
+		print(' - ' + page[tag_index + inc : endIndex])
+		tag_index = page.find(tag, tag_index + 1)
+
+# bio
+
+page = website.content.decode('utf-8')
+
+bioIndex = page.find('f4"')
+if page[bioIndex + 5] != 'h':
+	endBio = page.find('<', bioIndex + 11)
+	print(' - ' + page[bioIndex + 12 : endBio])
+
+# location
+
+extract_data('profile', 'Home location:', '"', 15)
+
+# followers
+
+print('\nfollowers:')
+
+tag = 'k" data-hovercard-type="user" data-hovercard-url="/users/'
+extract_data("followers", tag, "/", 57)
+
+# following
+
+print("\nfollowing:")
+
+tag = 'k" data-hovercard-type="user" data-hovercard-url="/users/'
+extract_data('following', tag, "/", 57)
+
+# repos
+
+print('\nrepos:')
+
+tag = '<a href="/' + name + '/'
+extract_data('repos', tag, '"', len(name) + 11)
+
+# stars
+
+print('\nstars:')
+
+tag = 'd-inline-block mb-1'
+extract_data('stars', tag, '"', 47)
